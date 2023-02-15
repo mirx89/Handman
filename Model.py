a2590c004d1671d955b29ca16547ba0487d400a0
mirko <mirko.salus@kehtna.edu.ee>
2023-02-15 21:21:17 +0200

    Valmis töö

--- Model.py
+++ Model.py
@@ -29,8 +29,8 @@ class Model:
         for x in range(len(self.new_word)):
             self.user_word.append("_")
 
-        print(self.new_word)  # test autojuht
-        print(self.user_word)  # test ["_", "_", "_",.....
+        #  print(self.new_word)  # test autojuht
+        #  print(self.user_word)  # test ["_", "_", "_",.....
 
     def get_random_word(self):
         connection = sqlite3.connect(self.database_name)  # create connection to database
@@ -43,18 +43,26 @@ class Model:
             user_char = userinput[:1]  # Only first letter
             if user_char.lower() in self.new_word.lower():
                 self.change_user_input(user_char)  # Find letter
+            elif user_char.upper() in self.all_user_chars:
+                self.counter += 1
             else:  # Letter not found
                 self.counter += 1
                 self.all_user_chars.append(user_char.upper())
+                # print(self.all_user_chars)  # Test
 
     def change_user_input(self, user_char):
         # Replace all "_" with found letter
         current_word = self.chars_to_list(self.new_word)
         x = 0
-        for c in current_word:
-            if user_char.lower() == c.lower():
-                self.user_word[x] = user_char.upper()
-            x += 1
+        # kontrolli kas kasutaja sisestatud täht on juba user_word listis. user_word list algselt ["_","_","_"]
+        # kui leitud siis user_word list ["A","_","_"], kui ei ole siis lisa täht ja kui on siis lisa +1 vale täht
+        if user_char.upper() not in self.user_word:
+            for c in current_word:
+                if user_char.lower() == c.lower():
+                    self.user_word[x] = user_char.upper()
+                x += 1
+        else:
+            self.counter += 1
 
     @staticmethod
     def chars_to_list(string):
